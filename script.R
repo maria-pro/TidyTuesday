b89f37f13d263a0b6b5cc566145d6c281bf24b47
Maria Prokofieva <maria.prokofieva@gmail.com>
2019-11-20 18:08:18 +1100

    up

---
+++ script.R
@@ -0,0 +1,7 @@
+library("readr")
+#instructions on how to copy
+nz_bird <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-19/nz_bird.csv")
+
+
+
+votes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-19/BOTY-votes-2019.csv")
