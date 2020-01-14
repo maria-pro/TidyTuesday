ec871c48db34440fae89e6f9e2da08f63d5b5160
Maria Prokofieva <maria.prokofieva@gmail.com>
2020-01-14 14:14:30 +1100

    up

--- avocado.R
+++ avocado.R
@@ -1,11 +1,20 @@
+install.packages("tibbletime")
+
+
 library(dplyr)
 library(readr)
 library(ggplot2)
+library(tibbletime)
+
 
 list.files("../data/") #list files from data folder
 
 #data<-read_csv("../data/avocado.csv") # from readr - faster than read.csv, read factors correctly
 data<-read.csv("../data/avocado.csv") #read factors as factors!
+data$Date<-as.Date(data$Date, "%Y-%m-%d") 
+
+data<-as_tbl_time(data, index=Date)
+
 
 #Date - The date of the observation
 #AveragePrice - the average price of a single avocado
@@ -18,23 +27,33 @@ data<-read.csv("../data/avocado.csv") #read factors as factors!
 #4770 - Total number of avocados with PLU 4770 sold
 
 levels(data$type)
-class(data$type)
+levels(data$region)
 
-ggplot(data)+geom_point(aes(AveragePrice, Total.Volume))
+
+
+
+ggplot(data)+geom_point(aes(AveragePrice, Date))
 
 ggplot(data, aes(x=AveragePrice, fill=type))+geom_density()+facet_wrap(~ type)
 
 ggplot(data, aes(x=AveragePrice, fill=type))+geom_density()+facet_wrap(~ type)+
   theme_minimal()+labs(title="Let's see") + theme (legend.position="bottom")
 
+#
+vol_type <- data %>% 
+  group_by(type) %>% 
+  summarise(avg.vol=mean(Total.Volume))  %>% mutate(pct=prop.table(avg.vol) * 100)
+
 
 organic<- data %>% 
   select(Date, AveragePrice, Total.Volume, type)%>%
-                          filter(type=="organic")%>%
-class(organic)
-install.packages("tibbletime")
-library(tibbletime)
+                          filter(type=="organic")
 
-organic$Date<-as.Date(organic$Date, "Y-m-d")
-organic<-as_tbl_time(organic, index=Date)
+
+organic2<-as_period(organic, period="monthly")
+#class(organic$Date)
+
+
+#organic<-as_tbl_time(organic, index=Date)
+
 
---
+++ tidytuesday.R
@@ -0,0 +1,27 @@
+df<-data
+levels(df$type)
+
+options(repr.plot.width=8, repr.plot.height=4)
+ggplot(df, aes(x=AveragePrice, fill=type)) + geom_density() + facet_wrap(~type) + theme_minimal() + 
+  theme(plot.title=element_text(hjust=0.5), legend.position="bottom") + labs(title="Avocado Price by Type") + scale_fill_brewer(palette="Set2")
+
+
+vol_type <- df %>% group_by(type) %>% summarise(avg.vol=mean(Total.Volume))  %>% mutate(pct=prop.table(avg.vol) * 100)
+
+
+install.packages("rcrossref")
+library(rcrossref)
+queries <- c("big data analytics", "information systems")
+results<-cr_works(filter = c(has_orcid = TRUE, from_pub_date = '2010-10-10', type="journal-article"), query="big data analytics")
+
+text<-results$data
+
+
+dateSearch<-seq(as.Date("2010/01/01"), by = "year", length.out = 5)
+
+
+
+
+
+################
+library(tidyverse)
