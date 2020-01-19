2de6caf8d49c243594938aef1943a5d9e7633bba
Maria Prokofieva <maria.prokofieva@gmail.com>
2020-01-19 22:28:38 +1100

    up

---
+++ example.R
@@ -0,0 +1,11 @@
+# city economy file
+library(tidyverse)
+theme_set (theme_light)
+
+
+big_epa_cars <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-10-15/big_epa_cars.csv") %>%
+  mutate(uses_electricity = ifelse(highwayE > 0, "Uses Electricity", "Doesn't Use Electricity"))
+
+
+big_epa_cars_alphab<-big_epa_cars %>% select(sort(colnames(big_epa_cars)))
+
